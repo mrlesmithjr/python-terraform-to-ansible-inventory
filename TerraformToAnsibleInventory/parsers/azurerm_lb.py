342aba282949e5b7b68e1b2c70e1c4289de90e3c
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-07-20 19:09:08 -0400

    Closes #12

--- TerraformToAnsibleInventory/parsers/azurerm_lb.py
+++ TerraformToAnsibleInventory/parsers/azurerm_lb.py
@@ -10,7 +10,11 @@ def parse(RESOURCE, TERRAFORM_LOAD_BALANCERS, TERRAFORM_PUBLIC_IPS):
             public_ip_address = pub_ip['ip_address']
     load_balancer.update({'location': raw_attrs['location'],
                           'name': raw_attrs['name'],
+                          'private_ip_address':
+                          raw_attrs['private_ip_address'],
                           'public_ip_address': public_ip_address,
+                          'resource_group_name':
+                          raw_attrs['resource_group_name'],
                           'sku': raw_attrs['sku'],
                           'type': 'azurerm_lb'})
     TERRAFORM_LOAD_BALANCERS.append(load_balancer)
