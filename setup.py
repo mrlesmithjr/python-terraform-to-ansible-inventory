69340f9d86fa04bb4211ce27274f1a79d9f6d904
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-07-19 22:04:45 -0400

    Updated setup information

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup
+from setuptools import find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='TerraformToAnsibleInventory',
-      version='0.2.0',
-      description='Generates Ansible inventory in YAML format by parsing the tfstate.',
+      version='0.2.1',
+      description='Consumes Terraform State and generates Ansible inventory.',
       url='https://github.com/mrlesmithjr/python-terraform-to-ansible-inventory',
       author='Larry Smith Jr.',
       author_email='mrlesmithjr@gmail.com',
       license='MIT',
-      packages=['TerraformToAnsibleInventory'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
       scripts=['bin/TerraformToAnsibleInventory'],
       include_package_data=True,
-      install_requires=[
-          'argparse', 'ast', 'jinja2', 'PyYaml'
-      ],
+      classifiers=(
+          "Programming Language :: Python :: 2",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ),
       zip_safe=False)
