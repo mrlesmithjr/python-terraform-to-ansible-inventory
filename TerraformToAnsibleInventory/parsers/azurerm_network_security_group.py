f9ed467500ab03a7d44656d84e42dcef927fa16d
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-07-23 15:19:38 -0400

    Refactored variables and parsing for Azure network security groups
    This was found to be too narrowly focused on just Azure when beginning 
    to look into AWS security groups. So I refactored the Azure security 
    groups to allow AWS security groups to follow a similar pattern.

--- TerraformToAnsibleInventory/parsers/azurerm_network_security_group.py
+++ TerraformToAnsibleInventory/parsers/azurerm_network_security_group.py
@@ -1,29 +1,75 @@
-import json
-import yaml
 from .. logging_config import setup as LoggingConfigSetup
 
 
-def parse(LOG_LEVEL, RESOURCE, TERRAFORM_NETWORK_SECURITY_GROUPS):
+def parse(LOG_LEVEL, RESOURCE, TERRAFORM_SECURITY_GROUPS):
     """Populate Azure network security group info."""
     LOGGER = LoggingConfigSetup(LOG_LEVEL)
     raw_attrs = RESOURCE['primary']['attributes']
     LOGGER.debug(raw_attrs)
-    SECURITY_RULE_IDS = dict()
+    SECURITY_RULES = []
+    SECURITY_GROUP = dict()
 
     for KEY, VALUE in raw_attrs.items():
         if 'security_rule' in KEY and '#' not in KEY:
-            RULE = KEY.split('.')
-            RULE_ID = RULE[1]
-            RULE_TYPE = RULE[2]
-            if RULE_ID not in SECURITY_RULE_IDS:
-                SECURITY_RULE_IDS.update({RULE_ID: dict()})
-            SECURITY_RULE_IDS[RULE_ID].update({RULE_TYPE: VALUE})
+            security_rule(raw_attrs, KEY, SECURITY_RULES)
 
-    NETWORK_SECURITY_GROUP = dict()
-    NETWORK_SECURITY_GROUP.update(
-        {'id': raw_attrs['id'],
-         'name': raw_attrs['name'],
-         'location': raw_attrs['location'],
-         'resource_group_name': raw_attrs['resource_group_name'],
-         'security_group_rules': SECURITY_RULE_IDS})
-    TERRAFORM_NETWORK_SECURITY_GROUPS.append(NETWORK_SECURITY_GROUP)
+    populate(raw_attrs, SECURITY_GROUP,
+             SECURITY_RULES, TERRAFORM_SECURITY_GROUPS)
+
+
+def security_rule(raw_attrs, KEY, SECURITY_RULES):
+    SECURITY_RULE = dict()
+    RULE_ID = KEY.split('.')[1]
+    RULE_ACCESS = ('security_rule.%s.access' % RULE_ID)
+    RULE_DESCRIPTION = ('security_rule.%s.description' % RULE_ID)
+    RULE_DESTINATION_ADDRESS_PREFIX = (
+        'security_rule.%s.destination_address_prefix' % RULE_ID)
+    RULE_DESTINATION_PORT_RANGE = (
+        'security_rule.%s.destination_port_range' % RULE_ID)
+    RULE_DIRECTION = ('security_rule.%s.direction' % RULE_ID)
+    RULE_NAME = ('security_rule.%s.name' % RULE_ID)
+    RULE_PRIORITY = ('security_rule.%s.priority' % RULE_ID)
+    RULE_PROTOCOL = ('security_rule.%s.protocol' % RULE_ID)
+    RULE_SOURCE_ADDRESS_PREFIX = (
+        'security_rule.%s.source_address_prefix' % RULE_ID)
+    RULE_SOURCE_PORT_RANGE = (
+        'security_rule.%s.source_port_range' % RULE_ID)
+    SECURITY_RULE.update(
+        {
+            'access': raw_attrs[RULE_ACCESS],
+            'description': raw_attrs[RULE_DESCRIPTION],
+            'destination_address_prefix':
+            raw_attrs[RULE_DESTINATION_ADDRESS_PREFIX],
+            'destination_port_range':
+            raw_attrs[RULE_DESTINATION_PORT_RANGE],
+            'direction': raw_attrs[RULE_DIRECTION],
+            'name': raw_attrs[RULE_NAME],
+            'priority': raw_attrs[RULE_PRIORITY],
+            'protocol': raw_attrs[RULE_PROTOCOL],
+            'source_address_prefix':
+            raw_attrs[RULE_SOURCE_ADDRESS_PREFIX],
+            'source_port_range': raw_attrs[RULE_SOURCE_PORT_RANGE]
+        }
+    )
+    if SECURITY_RULE not in SECURITY_RULES:
+        SECURITY_RULES.append(SECURITY_RULE)
+
+
+def populate(raw_attrs, SECURITY_GROUP, SECURITY_RULES, TERRAFORM_SECURITY_GROUPS):
+    SECURITY_GROUP.update(
+        {
+            'id': raw_attrs['id'],
+            'name': raw_attrs['name'],
+            'location': raw_attrs['location'],
+            'resource_group_name': raw_attrs['resource_group_name'],
+            'security_groups': [
+                {
+                    'name': raw_attrs['name'],
+                    'rules': SECURITY_RULES
+                }
+            ]
+        }
+    )
+
+    if SECURITY_GROUP not in TERRAFORM_SECURITY_GROUPS:
+        TERRAFORM_SECURITY_GROUPS.append(SECURITY_GROUP)
